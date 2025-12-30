c91d5f2d0372e8efa47c914d1ee4d62cd8c1761e
Ashu Singh <ashusingh2201@gmail.com>
2025-12-31 00:50:36 +0530

    added back motion Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import Navbar from "./Component/Nav/Navbar"
 import SecNav from "./Component/secondnav/SecNav"
+import { motion } from "motion/react"
+import assets from "./assets"
+import { useEffect, useState } from "react";
 
 function App() {
- 
+
+  const backImages=[assets.backimage1,assets.backimage2,assets.backimage3,assets.backimage4];
+
+  const [index,setIndex]=useState(0)
+
+  useEffect(()=>{
+    const timer=setInterval(()=>{
+      setIndex((p)=>(p+1) % backImages.length)
+    },4000)
+
+    return ()=>clearInterval(timer)
+  },[])
+
+
 
   return (
-    <div className='min-w-[1324px] max-h-100vh  bg-gray-500'>
+
+    <div>
       <Navbar/>
       <SecNav/>
+    <motion.div
+    animate={{backgroundImage:`url(${backImages[index]})`}}
+    transition={{duration:1}}
+    className='min-w-[1324px] max-h-100vh h-screen bg-cover bg-no-repeat bg-center'>
+      
+      
 
+    </motion.div>
     </div>
   )
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,18 @@
 import logo from './logo.png'
 import map from './map.jpg'
+import backimage1 from './backimage1.jpg'
+import backimage2 from './backimage2.jpg'
+import backimage3 from './backimage3.jpg'
+import backimage4 from './backimage4.jpg'
+
+
 
 const assets={
-  logo,map
+  logo,map,backimage1,backimage2,backimage3,backimage4
 }
 
 export const navItems=[{id:1,name:"ALL"},{id:2,name:"Amazon miniTV"},{id:3,name:"Sell"},{id:4,name:"Best Sellers"},{id:5,name:"Mobiles"},{id:6,name:"Customer Service"},{id:7,name:"Today's Deals"},{id:8,name:"Electronics"},{id:9,name:"Fashion"},{id:10,name:"New Releases"},{id:11,name:
 "Books"},{id:12,name:"Home & Kitchen"},{id:13,name:"Amazon Pay",},{id:14,name:"Computers"},{id:15,name:"Beauty"}]
 
 export default assets
+
