6d7e5431bba87cb4389012fec4908231ebcb0a74
Ashu Singh <ashusingh2201@gmail.com>
2026-01-02 18:25:18 +0530

    updated datas

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,16 +4,44 @@ import backimage1 from './backimage1.jpg'
 import backimage2 from './backimage2.jpg'
 import backimage3 from './backimage3.jpg'
 import backimage4 from './backimage4.jpg'
-import img1_1 from './img1_1'
-import img1_2 from './img1_2'
-import img1_3 from './img1_3'
-import img1_4 from './img1_4'
+import img1_1 from './img1_1.jpg'
+import img1_2 from './img1_2.jpg'
+import img1_3 from './img1_3.jpg'
+import img1_4 from './img1_4.jpg'
+import img2_1 from './img2_1.jpg'
+import img2_2 from './img2_2.jpg'
+import img2_3 from './img2_3.jpg'
+import img2_4 from './img2_4.jpg'
+import img3_1 from './img3_1.jpg'
+import img3_2 from './img3_2.jpg'
+import img3_3 from './img3_3.jpg'
+import img3_4 from './img3_4.jpg'
+import img4_1 from './img4_1.jpg'
+import img4_2 from './img4_2.jpg'
+import img4_3 from './img4_3.jpg'
+import img4_4 from './img4_4.jpg'
+import img5_1 from './img5_1.jpg'
+import img5_2 from './img5_2.jpg'
+import img5_3 from './img5_3.jpg'
+import img5_4 from './img5_4.jpg'
+import img6_1 from './img6_1.jpg'
+import img6_2 from './img6_2.jpg'
+import img6_3 from './img6_3.jpg'
+import img6_4 from './img6_4.jpg'
+import img7_1 from './img7_1.jpg'
+import img7_2 from './img7_2.jpg'
+import img7_3 from './img7_3.jpg'
+import img7_4 from './img7_4.jpg'
+import img8_1 from './img8_1.jpg'
+import img8_2 from './img8_2.jpg'
+import img8_3 from './img8_3.jpg'
+import img8_4 from './img8_4.jpg'
 
 
 
 
 const assets={
-  logo,map,backimage1,backimage2,backimage3,backimage4,img1_1,img1_2,img1_3,img1_4
+  logo,map,backimage1,backimage2,backimage3,backimage4,img1_1,img1_2,img1_3,img1_4,img2_1,img2_2,img2_3,img2_4,img3_1,img3_2,img3_3,img3_4,img4_1,img4_2,img4_3,img4_4,img5_1,img5_2,img5_3,img5_4,img6_1,img6_2,img6_3,img6_4,img7_1,img7_2,img7_3,img7_4,img8_1,img8_2,img8_3,img8_4
 }
 
 export const navItems=[{id:1,name:"ALL"},{id:2,name:"Amazon miniTV"},{id:3,name:"Sell"},{id:4,name:"Best Sellers"},{id:5,name:"Mobiles"},{id:6,name:"Customer Service"},{id:7,name:"Today's Deals"},{id:8,name:"Electronics"},{id:9,name:"Fashion"},{id:10,name:"New Releases"},{id:11,name:
@@ -38,10 +66,10 @@ export const boxes=[
     section:"Bulk order discounts + Up to 18% GST savings",
     type:"category",
     items:[
-      {id:1,image:assets.img1_1,text:"Up to 45% off | Laptops"},
-      {id:2,image:assets.img1_2,text:"Up to 60% off | Kitchen appliances"},
-      {id:3,image:assets.img1_3,text:"Min. 50% off | Office furniture"},
-      {id:4,image:assets.img1_4,text:"Up to 60% off | for Business Purchases"}
+      {id:1,image:assets.img2_1,text:"Up to 45% off | Laptops"},
+      {id:2,image:assets.img2_2,text:"Up to 60% off | Kitchen appliances"},
+      {id:3,image:assets.img2_3,text:"Min. 50% off | Office furniture"},
+      {id:4,image:assets.img2_4,text:"Up to 60% off | for Business Purchases"}
     ],
     links:"Create a free account"
   },
@@ -49,20 +77,20 @@ export const boxes=[
     section:"Appliances for your home | Up to 55% off",
     type:"category",
     items:[
-      {id:1,image:assets.img1_1,text:"Air conditioners"},
-      {id:2,image:assets.img1_2,text:"Refrigerator"},
-      {id:3,image:assets.img1_3,text:"Microwaves"},
-      {id:4,image:assets.img1_4,text:"Washing machines"}
+      {id:1,image:assets.img3_1,text:"Air conditioners"},
+      {id:2,image:assets.img3_2,text:"Refrigerator"},
+      {id:3,image:assets.img3_3,text:"Microwaves"},
+      {id:4,image:assets.img3_4,text:"Washing machines"}
     ],
     links:"See more"
   },
     {
     section:"Starting ₹49 | Deals on home essentials",
     items:[
-      {id:1,image:assets.img1_1,text:"Cleaning supplies"},
-      {id:2,image:assets.img1_2,text:"Bathroom accessories"},
-      {id:3,image:assets.img1_3,text:"Home tools"},
-      {id:4,image:assets.img1_4,text:"Wallpapers"}
+      {id:1,image:assets.img4_1,text:"Cleaning supplies"},
+      {id:2,image:assets.img4_2,text:"Bathroom accessories"},
+      {id:3,image:assets.img4_3,text:"Home tools"},
+      {id:4,image:assets.img4_4,text:"Wallpapers"}
     ],
     links:"Explore all"
   },
@@ -70,10 +98,10 @@ export const boxes=[
     section:"Starting ₹149 | Headphones",
     type:"category",
     items:[
-      {id:1,image:assets.img1_1,text:"Starting 249 | boAt"},
-      {id:2,image:assets.img1_2,text:"Starting 349 | boult"},
-      {id:3,image:assets.img1_3,text:"Starting 649 | Noise"},
-      {id:4,image:assets.img1_4,text:"Starting 149 Zebronics"}
+      {id:1,image:assets.img5_1,text:"Starting 249 | boAt"},
+      {id:2,image:assets.img5_2,text:"Starting 349 | boult"},
+      {id:3,image:assets.img5_3,text:"Starting 649 | Noise"},
+      {id:4,image:assets.img5_4,text:"Starting 149 Zebronics"}
     ],
     links:"See all offers"
   },
@@ -81,10 +109,10 @@ export const boxes=[
     section:"Automotive essentials | Up to 60% off",
     type:"category",
     items:[
-      {id:1,image:assets.img1_1,text:"Cleaning accessories"},
-      {id:2,image:assets.img1_2,text:"Tyre & rim care"},
-      {id:3,image:assets.img1_3,text:"Helmets"},
-      {id:4,image:assets.img1_4,text:"Vacuum cleaner"}
+      {id:1,image:assets.img6_1,text:"Cleaning accessories"},
+      {id:2,image:assets.img6_2,text:"Tyre & rim care"},
+      {id:3,image:assets.img6_3,text:"Helmets"},
+      {id:4,image:assets.img6_4,text:"Vacuum cleaner"}
     ],
     links:"Explore all"
   },
@@ -92,10 +120,10 @@ export const boxes=[
     section:"Starting ₹199 | Amazon Brands & more",
     type:"category",
     items:[
-      {id:1,image:assets.img1_1,text:"Starting 199 | Bedshees"},
-      {id:2,image:assets.img1_2,text:"Starting 199 | Curtains"},
-      {id:3,image:assets.img1_3,text:"Minimum 40% off | Ironing board & more"},
-      {id:4,image:assets.img1_4,text:"Up to 60% off | Home decor"}
+      {id:1,image:assets.img7_1,text:"Starting 199 | Bedshees"},
+      {id:2,image:assets.img7_2,text:"Starting 199 | Curtains"},
+      {id:3,image:assets.img7_3,text:"Minimum 40% off | Ironing board & more"},
+      {id:4,image:assets.img7_4,text:"Up to 60% off | Home decor"}
     ],
     links:"See more"
   },
@@ -103,10 +131,10 @@ export const boxes=[
     section:"Up to 50% off | Baby care & toys | Amazon Brands",
     type:"category",
     items:[
-      {id:1,image:assets.img1_1,text:"Up to 50% off | Baby diapers & wipes "},
-      {id:2,image:assets.img1_2,text:" Up to 50% off | Ride ons"},
-      {id:3,image:assets.img1_3,text:"Starting 649 | RC car"},
-      {id:4,image:assets.img1_4,text:"Up to 50% off | Baby safety essentials"}
+      {id:1,image:assets.img8_1,text:"Up to 50% off | Baby diapers & wipes "},
+      {id:2,image:assets.img8_2,text:" Up to 50% off | Ride ons"},
+      {id:3,image:assets.img8_3,text:"Starting 649 | RC car"},
+      {id:4,image:assets.img8_4,text:"Up to 50% off | Baby safety essentials"}
     ],
     links:"See all essentials"
   }
